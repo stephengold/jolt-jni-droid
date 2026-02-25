54924991fc1310dcf9c329a98078f170b2cbccdf
stephengold <sgold@sonic.net>
2026-02-24 20:16:10 -0800

    bump compileSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.github.stephengold.joltjni.droid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.github.stephengold.joltjni.droid"
