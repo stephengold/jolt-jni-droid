ef10e065696b92fd4553f1fd5efa9ac3a1b5e1a1
stephengold <sgold@sonic.net>
2026-03-08 23:25:46 -0700

    meld.sh:  re-write

--- bash/meld.sh
+++ bash/meld.sh
@@ -5,13 +5,23 @@ set -e
 GitDir=~/NetBeansProjects
 #GitDir="/c/Users/sgold/My Documents/NetBeansProjects"
 
-S1="$GitDir/jolt-jni-droid/HelloJoltJni/src/main/res"
-D1="$GitDir/jolt-jni-droid/SmokeTestAll/src/main/res"
+L1="$GitDir/jolt-jni-droid/HelloJoltJni"
+R1="$GitDir/jolt-jni-droid/SmokeTestAll"
 
-S2="$GitDir/jolt-jni-droid/HelloJoltJni/src/main/java/com/github/stephengold/joltjni/droid"
-D2="$GitDir/jolt-jni-droid/SmokeTestAll/src/main/java/com/github/stephengold/joltjni/droidsta"
+L2="$GitDir/jolt-jni-droid/HelloJoltJni/src/main/java/com/github/stephengold/joltjni/droid"
+R2="$GitDir/jolt-jni-droid/SmokeTestAll/src/main/java/com/github/stephengold/joltjni/droidsta"
+
+L3="$GitDir/jolt-jni-droid/HelloJoltJni/src/main/java/com/github/stephengold/joltjni/droid/HelloJoltJni.java"
+R3="$GitDir/LbjDroid/HelloLibbulletjme/src/main/java/com/github/stephengold/lbjdroid/HelloLibbulletjme.java"
+
+L4="$GitDir/jolt-jni-droid/HelloJoltJni"
+R4="$GitDir/LbjDroid/HelloLibbulletjme"
+
+L5="$GitDir/jolt-jni-droid/HelloJoltJni/src/main/java/com/github/stephengold/joltjni/droid"
+R5="$GitDir/LbjDroid/HelloLibbulletjme/src/main/java/com/github/stephengold/lbjdroid"
 
 Meld="/usr/bin/meld"
 #Meld="/c/Program Files/Meld/meld"
 
-"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2"
+"$Meld" --diff "$L1" "$R1" --diff "$L2" "$R2" --diff "$L3" "$R3" \
+        --diff "$L4" "$R4" --diff "$L5" "$R5"
