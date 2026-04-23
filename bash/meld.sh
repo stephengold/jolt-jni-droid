daf5f60f92aaf5c963e7cce7a0f3ab4ae86a5e7e
stephengold <sgold@sonic.net>
2026-04-22 17:22:54 -0700

    meld.sh:  also compare jolt-jni-droid/SmokeTestAll with Jolt JNI tests

--- bash/meld.sh
+++ bash/meld.sh
@@ -20,8 +20,11 @@ R4="$GitDir/LbjDroid/HelloLibbulletjme"
 L5="$GitDir/jolt-jni-droid/HelloJoltJni/src/main/java/com/github/stephengold/joltjni/droid"
 R5="$GitDir/LbjDroid/HelloLibbulletjme/src/main/java/com/github/stephengold/lbjdroid"
 
+L6="$GitDir/jolt-jni-droid/SmokeTestAll/src/main"
+R6="$GitDir/jolt-jni/src/test"
+
 Meld="/usr/bin/meld"
 #Meld="/c/Program Files/Meld/meld"
 
 "$Meld" --diff "$L1" "$R1" --diff "$L2" "$R2" --diff "$L3" "$R3" \
-        --diff "$L4" "$R4" --diff "$L5" "$R5"
+        --diff "$L4" "$R4" --diff "$L5" "$R5" --diff "$L6" "$R6"
